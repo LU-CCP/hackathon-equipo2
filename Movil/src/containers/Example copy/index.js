d717c9ae93cb4baf4008e5677f9139f8fb92dd5b
marcoac001 <marcoac001@lagash.com>
2020-01-17 09:42:01 -0300

    Nice

--- Movil/src/containers/Example copy/index.js
+++ Movil/src/containers/Example copy/index.js
@@ -1,11 +1,14 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, ImageBackground, Text } from 'react-native';
+import { Image, ImageBackground, Text } from 'react-native';
 
-import { Puntos as PuntosComponent } from '../../components';
+import {
+  Puntos as PuntosComponent,
+  Transmision as TransmisionComponent
+} from '../../components';
 import { HERMES_ENGINE } from '../../config/messages';
 import wallpaper from '../../resources/images/wallpaper.png';
+import Logo from '../../resources/images/Logo.png';
 import Main from '../Main';
 
 import styles from './styles';
@@ -20,19 +23,13 @@ const Example = ({ navigation }) => (
       {!!global.HermesInternal && (
         <Text style={styles.engine}>{HERMES_ENGINE}</Text>
       )}
-      <View style={{ width: 125, height: 125, flex: 1, flexDirection: 'row' }}>
-        <PuntosComponent />
-      </View>
-      <View
-        style={{
-          width: 125,
-          height: 125,
-          flex: 60,
-          alignItems: 'flex-end'
-        }}
-      >
-        <PuntosComponent />
-      </View>
+      <Image
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{ width: 370, height: 250 }}
+        source={Logo}
+      />
+      <PuntosComponent />
+      <TransmisionComponent />
     </ImageBackground>
   </Main>
 );
